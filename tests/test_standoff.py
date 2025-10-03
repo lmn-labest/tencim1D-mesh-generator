d56076448f8d84027678910dbb15ba0c22b7806a
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2025-10-03 18:45:53 -0300

    [FEAT: #1] - Syandoff rigido recebe os valores Dc e Gamma Maximo
    close #1

--- tests/test_standoff.py
+++ tests/test_standoff.py
@@ -4,17 +4,47 @@ from tencim1d_mesh_generator.standoff import StandoffFlexible, StandoffRigid
 
 
 @pytest.mark.parametrize(
-    'well_diameter,casing_external_diameter,sc,ratio',
+    'well_diameter,casing_external_diameter,dc,gamma,ratio',
     [
-        (2.0, 1.0, 0.4, 0.8),
-        (3.0, 1.0, 0.4, 0.4),
-        (0.1, 0.01, 0.01, 0.2222222222222222),
+        (2.0, 1.0, 1.5, 0.0, 0.5),
+        (3.0, 1.0, 1.0, 0.0, 0.0),
+        (0.1, 0.01, 0.05, 0.0, 0.4444444444444444),
+        (3.0, 1.0, 3.0, 0.2, 0.8),
+    ],
+    ids=['mesh-1', 'mesh-2', 'mesh-3', 'mesh-4'],
+)
+def test_standoff_rigid_ratio(well_diameter, casing_external_diameter, dc, gamma, ratio):
+    standoff = StandoffRigid(casing_external_diameter, well_diameter, dc, gamma)
+    assert standoff.ratio == pytest.approx(ratio)
+
+
+@pytest.mark.parametrize(
+    'well_diameter,casing_external_diameter,dc,sc',
+    [
+        (2.0, 1.0, 1.5, 0.25),
+        (3.0, 1.0, 1.0, 0.0),
+        (0.1, 0.01, 0.05, 0.02),
+        (3.0, 1.0, 3.0, 1.0),
+    ],
+    ids=['mesh-1', 'mesh-2', 'mesh-3', 'mesh-4'],
+)
+def test_standoff_rigid_sc(well_diameter, casing_external_diameter, dc, sc):
+    standoff = StandoffRigid(casing_external_diameter, well_diameter, dc)
+    assert standoff.sc == pytest.approx(sc)
+
+
+@pytest.mark.parametrize(
+    'well_diameter,casing_external_diameter,dc,la',
+    [
+        (2.0, 1.0, 1.5, 0.5),
+        (3.0, 1.0, 1.0, 1.0),
+        (0.1, 0.01, 0.05, 0.045),
     ],
     ids=['mesh-1', 'mesh-2', 'mesh-3'],
 )
-def test_standoff_rigid_ratio(well_diameter, casing_external_diameter, sc, ratio):
-    standoff = StandoffRigid(casing_external_diameter, well_diameter, sc)
-    assert standoff.ratio == pytest.approx(ratio)
+def test_standoff_rigid_ls(well_diameter, casing_external_diameter, dc, la):
+    standoff = StandoffRigid(casing_external_diameter, well_diameter, dc)
+    assert standoff.la == pytest.approx(la)
 
 
 @pytest.mark.parametrize(
